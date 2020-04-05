9df1a98576f42a1068f71270d6f649d61090b9fa
Zeid Adabel <zedjam98@hotmail.com>
2020-04-05 22:53:49 +0200

    base - has hotspot mechanics and movelook with mouse

---
+++ Assets/Code/Hotspot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hotspot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    internal void Hovered()
+    {
+        
+        transform.localScale = new Vector3(3, 3, 3);
+        transform.Find("name").gameObject.SetActive(true);
+    }
+    internal void DeHovered()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.Find("name").gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Code/SelectionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    private Hotspot currentlyHovered;
+    // Update is called once per frame
+    void Update()
+    {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit)){
+            var selection = hit.transform;
+            var hotSpot = selection.GetComponent<Hotspot>();
+            if(hotSpot != null)
+            {
+                hotSpot.Hovered();
+                currentlyHovered = hotSpot;
+            }
+        }
+        else
+        {
+            if (currentlyHovered != null)
+            {
+                currentlyHovered.DeHovered();
+            }
+        }
+    }
+}
